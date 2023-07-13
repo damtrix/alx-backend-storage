005b5b25f63a6e2baf39f53dd6b9e740ae320bf7
damtrix <oludamola.onarinde@gmail.com>
2023-07-13 14:56:25 +0100

    start

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- SQL script that creates a table users following these requirements:
+-- With these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255)
+)
